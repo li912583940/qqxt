da0c1e5f95bb098f65ff2d4b7daa6f5299c0504b
li912583940 <912583940@qq.com>
2019-02-28 16:56:37 +0800

    web

--- QqSystem/src/main/java/com/sl/ue/service/sys/SysUserGroupService.java
+++
@@ -1,8 +0,0 @@
-package com.sl.ue.service.sys;
-
-import com.sl.ue.entity.sys.vo.SysUserGroupVO;
-import com.sl.ue.service.base.BaseService;
-
-public interface SysUserGroupService extends BaseService<SysUserGroupVO>{
-
-}
